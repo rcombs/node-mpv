ba141e6ac5a0e83b0b3f51c8807c2988dc2af362
Rodger Combs <rodger.combs@gmail.com>
2015-04-15 07:48:01 -0500

    Now with Promises, single-quotes, and bound methods

--- test.js
+++ test.js
@@ -1,5 +1,5 @@
 var mpv = require('./');
-var x = new mpv({}, function(){
+var x = new mpv({'mpvArgs': ['--fs', '--force-window', '--no-osc']}, function(){
     x.sendMessage({command: ['client_name']}, function(res){
         console.log(res);
     });
